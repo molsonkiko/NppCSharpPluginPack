35a3cf3fd2341d9a8f7990f8630c720a4b9bbefd
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2024-12-29 10:53:24 -0800

    fix bug in notepad.GetOpenFileNames now it won't falsely list a "new 1" file for an invisible view     (if the mainView or subView is closed, it always has a dummy buffer even though it's not visible)

--- NppCSharpPluginPack/Properties/AssemblyInfo.cs
+++ NppCSharpPluginPack/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.3.16")]
-[assembly: AssemblyFileVersion("0.0.3.16")]
+[assembly: AssemblyVersion("0.0.3.17")]
+[assembly: AssemblyFileVersion("0.0.3.17")]
